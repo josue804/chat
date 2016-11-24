cac47d322d09a82f0576dfd63d891019f58ead0c
Josue Valverde <josuevalverde12@gmail.com>
2016-11-24 02:38:47 -0800

    changes static settings to recommended by heroku.

--- hpc/settings.py
+++ hpc/settings.py
@@ -134,9 +134,11 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'chat/static/chat/'),
-]
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static'),
+)

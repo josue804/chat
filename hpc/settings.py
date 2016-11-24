80d1b94bd0427a7b7b88815e930cc016692f0ac7
Josue Valverde <josuevalverde12@gmail.com>
2016-11-24 03:23:35 -0800

    adds database setting for heroku.

--- hpc/settings.py
+++ hpc/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.abspath(os.path.dirname(__name__))
@@ -89,16 +89,17 @@ WSGI_APPLICATION = 'hpc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hpc',
-        'USER': 'root',
-        'PASSWORD': 'meh',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hpc',
+#         'USER': 'root',
+#         'PASSWORD': 'meh',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
+DATABASES = {'default' : dj_database_url.config() }
 
 
 

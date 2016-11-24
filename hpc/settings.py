834979c5f8678baa39e718e4658b9117276aa0a9
Josue Valverde <josuevalverde12@gmail.com>
2016-11-24 02:31:22 -0800

    adds staticfiles_dirs in hopes that heroku will find static files.

--- hpc/settings.py
+++ hpc/settings.py
@@ -135,5 +135,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = 'chat/'
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, '/static/')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'chat/static/chat/'),
+]

80a2e5538653797f1af26318ecca31717be0f1e2
Josue Valverde <josuevalverde12@gmail.com>
2017-04-13 10:23:39 -0700

    Makes asgi and wsgi use production settings in production

--- website/asgi.py
+++ website/asgi.py
@@ -1,5 +1,9 @@
 import os
 import channels.asgi
+from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.base")
+if settings.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.base")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.production")
 channel_layer = channels.asgi.get_channel_layer()
--- website/wsgi.py
+++ website/wsgi.py
@@ -11,8 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.base")
+if settings.DEBUG:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.base")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)

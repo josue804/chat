d108167adc13550eb1559c29affcc8f69f4da64c
Josue Valverde <josuevalverde12@gmail.com>
2017-04-13 10:32:27 -0700

    Modifies manage script to use production settings in production

--- manage.py
+++ manage.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.conf import settings
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.base")
+    if settings.DEBUG:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.base")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings.production")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:

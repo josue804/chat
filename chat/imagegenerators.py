726ddcede12147d2a012e940f1a1e7cec86eb7cb
Josue Valverde <josuevalverde12@gmail.com>
2017-05-03 10:28:13 -0700

    Includes missing imports for custom s3 boto storage

--- chat/imagegenerators.py
+++ chat/imagegenerators.py
@@ -1,7 +1,9 @@
+import os
 from pilkit.lib import Image, ImageDraw
 from imagekit import ImageSpec, register
 from imagekit.processors import ResizeToFill, ResizeToFit, SmartResize
 from storages.backends.s3boto3 import S3Boto3Storage
+from tempfile import SpooledTemporaryFile
 
 
 class UserAccountAvatar(ImageSpec):

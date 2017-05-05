f8b342a332350e417d597fff2b498f7d98a35674
Josue Valverde <josuevalverde12@gmail.com>
2017-05-05 10:05:00 -0700

    Makes cookies secure

--- website/settings/production.py
+++ website/settings/production.py
@@ -5,4 +5,6 @@ DEBUG = False
 SECRET_KEY = os.environ.get('HPC_SECRET_KEY')
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

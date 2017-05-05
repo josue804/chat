eb67aae2db122c55a9a0588edc91d0a8bd2635d0
Josue Valverde <josuevalverde12@gmail.com>
2017-05-04 23:25:34 -0700

    Removes links to user profiles for guest users

--- chat/models/chat.py
+++ chat/models/chat.py
@@ -6,6 +6,8 @@ from chat.extras import get_local_time
 from .auth import CustomUser
 from django.utils.safestring import mark_safe
 from django.core.urlresolvers import reverse
+from lazysignup.utils import is_lazy_user
+
 
 # Create your models here.
 class Room(models.Model):
@@ -45,4 +47,7 @@ class Message(models.Model):
 
     @property
     def formatted_handle(self):
-        return mark_safe('<a href="' + reverse('account-detail', kwargs={'pk': self.user.pk}) + '" target="_blank">' + self.handle + '</a>' + ' on ' + timezone.localtime(self.timestamp).strftime('%b %-d %-I:%M %p PST'))
+        if is_lazy_user(self.user):
+            return self.handle + ' on ' + timezone.localtime(self.timestamp).strftime('%b %-d %-I:%M %p PST')
+        else:
+            return mark_safe('<a href="' + reverse('account-detail', kwargs={'pk': self.user.pk}) + '" target="_blank">' + self.handle + '</a>' + ' on ' + timezone.localtime(self.timestamp).strftime('%b %-d %-I:%M %p PST'))

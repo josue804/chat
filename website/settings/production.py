a9044b5753add8f676a4cd2287271b2e9debfba2
Josue Valverde <josuevalverde12@gmail.com>
2017-05-05 10:00:32 -0700

    Enables SSL on production server

--- website/settings/production.py
+++ website/settings/production.py
@@ -3,4 +3,6 @@ from .base import *
 
 DEBUG = False
 SECRET_KEY = os.environ.get('HPC_SECRET_KEY')
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

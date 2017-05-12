74454ada42afd7e84724c3df394693627925d799
Josue Valverde <josuevalverde12@gmail.com>
2017-05-11 23:19:24 -0700

    Restyles autocompletes

--- chat/static/chat/js/chat-dashboard.js
+++ chat/static/chat/js/chat-dashboard.js
@@ -1,31 +1,37 @@
-if ($('#condition').length > 0) {
-  $('document').ready(function(){
-    var $roomResults = $('#room-search-results');
-    var $roomResultsList = $('#room-search-results-list');
+$(function() {
 
-    $('#condition').autocomplete({
-      source: function(request, response){
-        if (request.term != "") {
-          data = $.ajax({
-            url: window.location.origin + '/chat/room-autocomplete/' + request.term + '/',
-            dataType: "json",
-            success: function(conditions) {
-              response($.map( conditions.splice(0,19), function(condition){
-                return {
-                  label: condition.fields.name,
-                  extra: {'connections': condition.fields.connections}
-                };
-              }));
-            }
-          });
+  if ($('#condition').length > 0) {
+    $('document').ready(function(){
+      var $roomResults = $('#room-search-results');
+      var $roomResultsList = $('#room-search-results-list');
+
+      $('#condition').autocomplete({
+        classes: {
+          'ui-autocomplete': 'custom-autocomplete',
+        },
+        source: function(request, response){
+          if (request.term != "") {
+            data = $.ajax({
+              url: window.location.origin + '/chat/room-autocomplete/' + request.term + '/',
+              dataType: "json",
+              success: function(conditions) {
+                response($.map( conditions.splice(0,19), function(condition){
+                  return {
+                    label: condition.fields.name,
+                    extra: {'connections': condition.fields.connections}
+                  };
+                }));
+              }
+            });
+          }
         }
-      }
-    }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-      return $( "<li></li>" )
-      .data( "item.autocomplete", item )
-      .append('<a>' + item.label + '</a>')
-      .append(item.extra.connections ? '<span> | ' + item.extra.connections + ' Active User(s)</span>' : '')
-      .appendTo(ul);
-    };
-  });
-};
+      }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+        return $( "<li></li>" )
+        .data( "item.autocomplete", item )
+        .append('<a>' + item.label + '</a>')
+        .append(item.extra.connections ? '<span> | ' + item.extra.connections + ' Active User(s)</span>' : '')
+        .appendTo(ul);
+      };
+    });
+  };
+});
